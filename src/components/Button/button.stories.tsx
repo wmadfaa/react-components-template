b574d71b9b65832a7dadda101acc6f8d890ece62
Wasim Almadfaa <wm.madfaa@gmail.com>
2019-05-11 13:41:40 +0200

    add addons

---
+++ src/components/Button/button.stories.tsx
@@ -0,0 +1,12 @@
+import { checkA11y } from "@storybook/addon-a11y";
+import { storiesOf } from "@storybook/react";
+import * as React from "react";
+import Button from "./Button";
+storiesOf("Button", module)
+  .addDecorator(checkA11y)
+  .add("with text", () => <Button>Hello Button</Button>)
+  .add("with some emoji", () => (
+    <Button styles={{ backgroundColor: "red", color: "darkRed" }}>
+      😀 😎 👍 💯
+    </Button>
+  ));
